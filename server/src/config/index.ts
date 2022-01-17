3282c60286c94247a53a2abc2d73e3a3f0f499db
Chicle <chicle@protonmail.com>
2022-01-17 13:14:46 +0100

    Actualización de la configuración y del .env

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -4,6 +4,7 @@ dotenv.config();
 export default {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3000,
-  DB_URI: process.env.DB_URI || 'mongodb://localhost:27017/url_shortener',
+  MONGO_URI: process.env.DB_URI || 'mongodb://localhost:27017/url_shortener',
+  MONGO_DB: process.env.DB_NAME || 'url_shortener',
   SECRET: process.env.SECRET || 'url_shortener_secret',
 };
