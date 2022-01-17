8ce819fdb0615afc93206c872717be7270d14ac8
Chicle <chicle@protonmail.com>
2022-01-17 13:10:59 +0100

    Creación de la aplicación en express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,12 @@
-function Test(msg: String){
-  console.log(`Test: ${msg}`);
-}
+import express from 'express';
+import Config from './config';
 
-Test("Hello from TypeScript");
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({message: "Navega, velero mío, sin temor que ni enemigo navío, ni tormenta, ni bonanza tu rumbo a torcer alcanza, ni a sujetar tu valor."});
+});
+
+app.listen(Config.PORT, () => {
+  console.log(`Server is listening on port ${Config.PORT}`);
+});
