e2a10d64bfc7b51df360d13f13d35e520e2a8159
Chicle <chicle@protonmail.com>
2022-01-17 13:29:10 +0100

    Creada conexión con la base de datos

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { connect } from './database';
 import Config from './config';
 
 const app = express();
+connect(); // Database connection
 
 app.get('/', (req, res) => {
   res.json({message: "Navega, velero mío, sin temor que ni enemigo navío, ni tormenta, ni bonanza tu rumbo a torcer alcanza, ni a sujetar tu valor."});
