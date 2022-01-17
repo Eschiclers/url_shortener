77bf7873c31220528b65daa2e7d3c3feb8e0d621
Chicle <chicle@protonmail.com>
2022-01-17 13:06:19 +0100

    Archivo de configuraciones

---
+++ server/src/config/index.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 3000,
+  DB_URI: process.env.DB_URI || 'mongodb://localhost:27017/url_shortener',
+  SECRET: process.env.SECRET || 'url_shortener_secret',
+};
