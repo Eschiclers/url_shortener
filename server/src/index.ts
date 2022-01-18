4dd00c22a74bb41b8c965126199983041d15266b
Chicle <chicle@protonmail.com>
2022-01-18 15:12:34 +0100

    Instalación de morgan

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import { connect } from './database';
 import Config from './config';
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
+app.use(morgan('dev'));
 
 // ROUTES
 
