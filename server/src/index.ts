864b53c1cde59fc8a7e637efe88f09177eb1e545
Chicle <chicle@protonmail.com>
2022-01-18 16:10:49 +0100

    Usar morgan si está en modo development

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
-app.use(morgan('dev'));
+
+if (Config.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // ROUTES
 
