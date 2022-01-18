948eac6d6308ddf6d17b0df0fbcf3facb23616ec
Chicle <chicle@protonmail.com>
2022-01-18 13:34:56 +0100

    Comprobar si existe el archivo .env

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,5 +1,11 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: `${__dirname}/../../.env` });
+import fs from 'fs';
+
+const ENV_FILE = `${__dirname}/../../.env`;
+
+if(!fs.existsSync(ENV_FILE)){ throw new Error('No .env file found, please create one. You can copy the example from .env-example file.'); }
+
+dotenv.config({ path: ENV_FILE });
 
 export default {
   NODE_ENV: process.env.NODE_ENV,
